cdfbba3a14455fae466e8c47a2a0b680e35d8a2a
Bryce Pilcher <bcpilche@ncsu.edu>
2015-09-25 11:48:09 -0400

    added default switch.

--- src/org/bcpilche/scanner/MetaToken.java
+++ src/org/bcpilche/scanner/MetaToken.java
@@ -2,70 +2,79 @@ package org.bcpilche.scanner;
 
 import java.io.PrintWriter;
 
-public class MetaToken implements Token{
+public class MetaToken implements Token {
 
-	TokenType type = TokenType.META;
-	String token = "";
-	int state = 0;
-	
-	@Override
-	public TokenType getTokenType() {
-		return type;
-	}
-	
-	@Override
-	public void Print(PrintWriter outputFile) {
-		outputFile.println(token);
-	}
-	
-	@Override
-	public String getToken() {
-		return token;
-	}
-	
-	public boolean match(char c){
-		token += c;
-		switch (state) {
-		case 0:
-			if(token.indexOf('#') == 0){
-				state = 2;
-			}else if(token.indexOf('/') == 0){
-				state = 1;
-			}else{
-				state = -1;
-			}
-			break;
-			
-		case 1:
-			if(token.lastIndexOf('/') == 1){
-				state = 2;
-			}else{
-				state = -1;
-			}
-			break;
-		
-		case 2:
-			state = 2;
-			break;
-		}
-		
-		if(state > 0){
-			return true;
-		}
-		else{
-			return false;
-		}
-	}
-	
-	@Override
-	public boolean match(String token) {
-		for(char c : token.toCharArray()){
-			if(!match(c)){
-				return false;
-			}
-		}
-		
-		this.token = token;
-		return true;
-	}
+    TokenType type = TokenType.META;
+    String token = "";
+    int state = 0;
+
+    @Override
+    public TokenType getTokenType() {
+        return type;
+    }
+
+    @Override
+    public void Print(PrintWriter outputFile) {
+        outputFile.println(token);
+    }
+
+    @Override
+    public String getToken() {
+        return token;
+    }
+
+    public boolean match(char c) {
+        token += c;
+        switch (state) {
+            case 0:
+                if (token.indexOf('#') == 0) {
+                    state = 2;
+                } else if (token.indexOf('/') == 0) {
+                    state = 1;
+                } else {
+                    state = -1;
+                }
+                break;
+
+            case 1:
+                if (token.lastIndexOf('/') == 1) {
+                    state = 2;
+                } else {
+                    state = -1;
+                }
+                break;
+
+            case 2:
+                state = 2;
+                break;
+
+            default:
+                if (token.indexOf('#') == 0) {
+                    state = 2;
+                } else if (token.indexOf('/') == 0) {
+                    state = 1;
+                } else {
+                    state = -1;
+                }
+                break;
+        }
+
+        if (state > 0) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    @Override
+    public boolean match(String token) {
+        for (char c : token.toCharArray()) {
+            if (!match(c)) {
+                return false;
+            }
+        }
+
+        this.token = token;
+        return true;
+    }
 }
